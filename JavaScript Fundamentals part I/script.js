684100ba01cc2856d2d76c2ffa7bfe910281f70c
MaFomedanu <constantinliviudaniel@yahoo.com>
2023-07-05 12:44:44 +0300

    added 4th challenge

--- JavaScript Fundamentals part I/script.js
+++ JavaScript Fundamentals part I/script.js
@@ -288,3 +288,139 @@ winnerBtnEl.addEventListener("click", () => {
 });
 
 /*****************************************************************************************/
+
+// Switch Statements
+
+// const day = "Monday";
+// // const day = "Thursday";
+// // const day = "Monashf";
+
+// switch (day) {
+//   case "Monday": // day === "Monday"
+//     console.log("Plan course structure");
+//     console.log("Go to coding meetup");
+//     break;
+
+//   case "Tuesday":
+//     console.log("Prepare theory videos");
+//     break;
+
+//   case "Wednesday":
+//   case "Thursday":
+//     console.log("Write code examples");
+//     break;
+
+//   case "Friday":
+//     console.log("Record video");
+//     break;
+
+//   case "Saturday":
+//   case "Sunday":
+//     console.log("Enjoy the weekend!");
+//     break;
+
+//   default:
+//     console.log("Not a valid day!");
+// }
+
+// // switch as if/else
+
+// if (day === "Monday") {
+//   console.log("Plan course structure");
+//   console.log("Go to coding meetup");
+// } else if (day === "Tuesday") {
+//   console.log("Prepare theory videos");
+// } else if (day === "Wednesday" || day === "Thursday") {
+//   console.log("Write code examples");
+// } else if (day === "Friday") {
+//   console.log("Record video");
+// } else if (day === "Saturday" || day === "Sunday") {
+//   console.log("Enjoy the weekend!");
+// } else {
+//   console.log("Not a valid day!");
+// }
+
+/*****************************************************************************************/
+
+// Statements and Expressions
+
+// // 3+4 is an expression as it produces a value
+// // 1991 is an expression as it will produce a value
+// // true && false && !false will eventually produce a boolean value
+
+// // statements are a sequence of actions, expressions are like the words  which make up a sentence
+
+// // an if statement doesn't produce a value, it only declares the value of a variable (in this particular case)
+// // it only performs an action
+// if (23 > 10) {
+//   const str = "23 is bigger";
+// }
+
+// console.log(`I'm ${2037 - 1991} years old.`);
+// // an expression
+
+// // console.log(`I'm ${if (23 > 10) {
+// //     const str = "23 is bigger";
+// //   }} years old`);
+// // a statement which can't be used to substitute an expression. As it doesn't produce a value it can't substitute expressions
+
+// const me = "Jonas";
+// console.log(`I'm ${2037 - 1991} years old. My name is ${me}`);
+// // as a variable has value it can be used in the above mentioned example
+
+/*****************************************************************************************/
+
+// The Conditional (Ternary) Operator
+
+// // const age = 23;
+// const age = 15;
+
+// // condition
+// age >= 18
+//   ? // if part
+//     console.log("I like drinking wine 🍷")
+//   : // else part
+//     console.log("I like drinking juice 🥤");
+
+// // an operator is an expression as it produces value
+// const drink = age >= 18 ? "wine 🍷" : "juice 🥤";
+// console.log(drink);
+
+// // Ternary operator as an if/else statement
+// let drink2;
+// if (age >= 18) {
+//   drink2 = "wine 🍷";
+// } else {
+//   drink2 = "juice 🥤";
+// }
+
+// console.log(drink2);
+
+// // ternary operator usage in a template literal
+// let age2 = 25;
+// console.log(`I like to drink ${age2 >= 18 ? "wine 🍷" : "juice 🥤"}`);
+
+/*****************************************************************************************/
+// CHALLENGE #4
+
+const billAmountEl = document.querySelector(".restaurant-bill input");
+const tipCalcBtnEl = document.querySelector(".tip-calc-btn");
+const fullAmountEl = document.querySelector(".full-amount");
+
+tipCalcBtnEl.addEventListener("click", () => {
+  if (!billAmountEl.value) {
+    document.querySelector(".error-msg3").style.opacity = 1;
+    fullAmountEl.textContent = "\u00A0";
+    return;
+  } else {
+    let bill = Number(billAmountEl.value);
+    const tip = 300 > bill && bill > 50 ? 0.15 * bill : 0.2 * bill;
+    document.querySelector(".error-msg3").style.opacity = 0;
+    billAmountEl.value = "";
+    fullAmountEl.textContent = `The bill was ${bill}$, the tip was ${tip}$ and the total value ${
+      bill + tip
+    }$.`;
+  }
+});
+
+/*****************************************************************************************/
