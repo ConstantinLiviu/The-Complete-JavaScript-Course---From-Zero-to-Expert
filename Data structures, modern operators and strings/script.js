489cbde966775bed1d65f4f69d80b79b556ad411
MaFomedanu <constantinliviudaniel@yahoo.com>
2023-08-14 23:42:16 +0300

    chalenge #2 taks 1 completed

--- Data structures, modern operators and strings/script.js
+++ Data structures, modern operators and strings/script.js
@@ -898,8 +898,55 @@ for (const [key, { open, close }] of entries) {
 
 // ****************************************************************************************************************** //
 // CHALLENGE #2
+// As it was somehow addressed in my solution for the first challenge I will only be writing the solutions here without additional implementation
 // 1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+const game3 = {
+  team1: "Bayern Munich",
+  team2: "Borrusia Dortmund",
+  players: [
+    [
+      " Neuer",
+      " Pavard",
+      " Martinez",
+      " Alaba",
+      " Davies",
+      " Kimmich",
+      " Goretzka",
+      " Coman",
+      " Muller",
+      " Gnarby",
+      " Lewandowski",
+    ],
+    [
+      " Burki",
+      " Schulz",
+      " Hummels",
+      " Akanji",
+      " Hakimi",
+      " Weigl",
+      " Witsel",
+      " Hazard",
+      " Brandt",
+      " Sancho",
+      " Gotze",
+    ],
+  ],
+  team1Subs: [" Thiago", " Coutinho", " Perisic"],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    X: 3.25,
+    team2: 6.5,
+  },
+};
+
+for (const [i, player] of game3.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
 // 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+
 // 3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
 //       Odd of victory Bayern Munich: 1.33
 //       Odd of draw: 3.25
