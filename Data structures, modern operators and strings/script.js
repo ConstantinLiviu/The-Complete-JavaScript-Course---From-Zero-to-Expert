9f4ea8f7c54d54cb568bdc85ac61e421cd5d04ac
MaFomedanu <constantinliviudaniel@yahoo.com>
2023-08-18 01:29:10 +0300

    challenge #5 completed

--- Data structures, modern operators and strings/script.js
+++ Data structures, modern operators and strings/script.js
@@ -1576,3 +1576,46 @@ function populateList(arr) {
     stringConversionEl.appendChild(newP);
   }
 }
+
+// ****************************************************************************************************************** //
+// CHALLENGE #5 - String Methods Practice
+// no UI as there is no user input
+
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+const separateFlights = flights.split("+");
+console.log(separateFlights);
+
+let flightsDestructured = [];
+for (const flight of separateFlights) {
+  const segments = flight.split(";");
+  flightsDestructured.push(
+    `${segments[0].includes("Delayed") ? "🔴" : ""} ${segments[0]
+      .replaceAll("_", " ")
+      .trimStart()} from ${segments[1]
+      .slice(0, 3)
+      .toUpperCase()} to ${segments[2]
+      .slice(0, 3)
+      .toUpperCase()} (${segments[3].replace(":", "h")})`.padStart(45)
+  );
+}
+console.log(flightsDestructured);
+
+// ALT SOLUTION
+const getCode = (str) => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split("+")) {
+  const [type, from, to, time] = flight.split(";");
+  const flightOutput = `${
+    type.startsWith("_Delayed") ? "🔴" : ""
+  } ${type.replaceAll("_", " ")} from ${getCode(from)} to ${getCode(
+    to
+  )} ${time.replace(":", "h")}`.padStart(45);
+  console.log(flightOutput);
+}
