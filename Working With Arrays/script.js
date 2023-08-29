24f2e52f77659d9ec0d2ce68cfbf34e4d207f9dc
MaFomedanu <constantinliviudaniel@yahoo.com>
2023-08-29 23:08:30 +0300

    find method lesson

--- Working With Arrays/script.js
+++ Working With Arrays/script.js
@@ -244,6 +244,52 @@ console.log(maxValue);
 
 //
 /* ****************************************************************************************************** */
+//  LESSON - THE FIND METHOD
+//
+
+const account1 = {
+  owner: "John Gamble",
+  transactions: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: "Millie Melvin",
+  transactions: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: "Jack Jacob Johanson",
+  transactions: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: "Serena Davis",
+  transactions: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// finds an element in an array based on a stated condition
+// will not return a new array; returns the first element in the array that satisfies the condition
+movements.find((movement) => movement < 0);
+
+const firstWithdrawal = movements.find((movement) => movement < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+const account = accounts.find((acc) => acc.owner === "John Gamble");
+console.log(account);
+
+//
+/* ****************************************************************************************************** */
 //  CHALLENGE #1
 //
 
