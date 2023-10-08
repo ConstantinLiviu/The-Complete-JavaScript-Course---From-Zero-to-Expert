2a896d4162952bfc99457d156e80a0d8c73e1067
MaFomedanu <constantinliviudaniel@yahoo.com>
2023-10-09 00:45:08 +0300

    challenge #4 completed

--- Object Oriented Programming/script.js
+++ Object Oriented Programming/script.js
@@ -352,6 +352,58 @@ chargeCh3Btn.addEventListener("click", function () {
 
 // DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
 
+class CarCh4 {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} speed is ${this.speed} km/h`);
+    return this;
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} speed is ${this.speed} km/h`);
+    return this;
+  }
+}
+
+class EVCl extends CarCh4 {
+  #charge;
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    console.log(`${this.make} battery charge is ${this.#charge}%`);
+    return this;
+  }
+  accelerate() {
+    this.speed += 20;
+    this.#charge -= 1;
+    console.log(
+      `${this.make} speed is ${this.speed} km/h with a charge of ${
+        this.#charge
+      }%`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl("Rivian", 120, 23);
+console.log(rivian);
+rivian.accelerate();
+console.log(rivian.speed);
+rivian.brake();
+console.log(rivian.speed);
+
+rivian.accelerate().accelerate().brake().chargeBattery(50);
+console.log(rivian);
 //
 // *******************************************************************************************************************//
 // LESSON - What is OOP?
