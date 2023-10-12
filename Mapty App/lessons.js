8b7ebeae4bdd65a88f0a01c5b2d1d9c5b6fa5bc4
MaFomedanu <constantinliviudaniel@yahoo.com>
2023-10-12 11:41:17 +0300

    geolocation API and display map/marker lessons

---
+++ Mapty App/lessons.js
@@ -0,0 +1,92 @@
+"use strict";
+
+//
+// ************************************************************************************************************************ //
+// LESSON - Geolocation API
+//
+
+// takes in 2 callback functions - success and failure
+// navigator.geolocation.getCurrentPosition(
+//   function (position) {
+//     console.log(position);
+//   },
+//   function () {
+//     alert("Could not get your position");
+//   }
+// );
+
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(
+//     function (position) {
+//       const { latitude } = position.coords;
+//       const { longitude } = position.coords;
+//       //   console.log(latitude, longitude);
+//       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+//     },
+//     function () {
+//       alert("Could not get your position");
+//     }
+//   );
+// }
+
+//
+// ************************************************************************************************************************ //
+// LESSON - Displaying a Map Using a Leaflet Library
+// LESSON - Displaying a Map Marker
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      //   console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      const coordinates = [latitude, longitude];
+
+      const map = L.map("map").setView(coordinates, 15);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      // L.marker(coordinates)
+      //   .addTo(map)
+      //   .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+      //   .openPopup();
+
+      console.log(map);
+
+      map.on("click", function (mapEvent) {
+        console.log(mapEvent.latlng);
+        const { lat, lng } = mapEvent.latlng;
+
+        console.log(lat);
+        const coords = [lat, lng];
+
+        L.marker(coords)
+          .addTo(map)
+          .bindPopup(
+            L.popup([coords], {
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: "running-pop-up",
+            })
+          )
+          .setPopupContent("Workout")
+          .openPopup();
+      });
+    },
+    function () {
+      alert("Could not get your position");
+    }
+  );
+}
+
+//
+// ************************************************************************************************************************ //
+//
+//
