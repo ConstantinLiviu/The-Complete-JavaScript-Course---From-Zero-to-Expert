240326f4f7973d4b074f8305a58e88af249c080c
MaFomedanu <constantinliviudaniel@yahoo.com>
2023-10-23 16:17:04 +0300

    GeoJournal map - extended features init

--- Mapty App/script.js
+++ Mapty App/script.js
@@ -1,35 +1,3 @@
 "use strict";
 
 // TASK - get user location
-
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      const { latitude } = position.coords;
-      const { longitude } = position.coords;
-
-      const coordinates = [latitude, longitude];
-
-      const map = L.map("map").setView(coordinates, 15);
-
-      // L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      //   attribution:
-      //     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      // }).addTo(map);
-
-      // Google maps style
-      L.tileLayer("http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
-        maxZoom: 20,
-        subdomains: ["mt0", "mt1", "mt2", "mt3"],
-      }).addTo(map);
-
-      L.marker(coordinates)
-        .addTo(map)
-        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-        .openPopup();
-    },
-    function () {
-      alert("Could not get your position");
-    }
-  );
-}
