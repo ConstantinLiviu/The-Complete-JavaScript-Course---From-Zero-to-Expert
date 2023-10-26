2a993d946322ae59da7edddf5586bab98783e53b
MaFomedanu <constantinliviudaniel@yahoo.com>
2023-10-26 22:14:05 +0300

    ajax request function completed

--- Asynchronous Javascript, AJAX and APIs/script.js
+++ Asynchronous Javascript, AJAX and APIs/script.js
@@ -11,31 +11,47 @@ const continentEl = document.querySelector(".continent");
 const populationEl = document.querySelector(".population");
 const languageEl = document.querySelector(".language");
 const currencyEl = document.querySelector(".currency");
+const appContainerEl = document.querySelector(".app-container");
 
-// Old school way
-const request = new XMLHttpRequest();
-request.open("GET", "https://restcountries.com/v3.1/name/romania");
-request.send();
+// // Old school way
+// const request = new XMLHttpRequest();
+// request.open("GET", "https://restcountries.com/v3.1/name/romania");
+// request.send();
 
-request.addEventListener("load", function () {
-  console.log(this);
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
 
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
+  request.addEventListener("load", function () {
+    console.log(this);
 
-  const html = `
-  <div class="card-container">
-    <div class="flag"></div>
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    const html = document.createElement("div");
+    html.classList.add("card-container");
+    html.innerHTML = `
+    <img class="flag" src="${data.flags.svg}" alt="${data.flags.alt}"/>
     <div class="country-data">
         <div class="country-name">
-            <h2 class="name"></h2>
-            <h3 class="continent"></h3>
+            <h2 class="name">${data.name.official}</h2>
+            <h3 class="continent">${data.region}</h3>
         </div>
         <div class="country-data">
-            <p class="population"></p>
-            <p class="language"></p>
-            <p class="currency"></p>
+            <p class="population">${(+data.population / 1000000).toFixed(1)}</p>
+            <p class="language">${Object.values(data.languages)[0]}</p>
+            <p class="currency">${
+              Object.entries(Object.values(data.currencies)[0])[0][1]
+            }</p>
         </div>
-    </div>
-  </div>`;
-});
+    </div>`;
+
+    appContainerEl.appendChild(html);
+  });
+};
+
+getCountryData("romania");
+getCountryData("spain");
+getCountryData("portugal");
+getCountryData("usa");
