c708dcd466051056886970a6ba6dec3dda11cfec
MaFomedanu <constantinliviudaniel@yahoo.com>
2023-10-28 22:59:15 +0300

    nested callbacks lesson

--- Asynchronous Javascript, AJAX and APIs/script.js
+++ Asynchronous Javascript, AJAX and APIs/script.js
@@ -59,3 +59,69 @@ getCountryData("romania");
 getCountryData("spain");
 getCountryData("portugal");
 getCountryData("usa");
+
+//
+// ************************************************************************************************************************* //
+// LESSON - Callback Hell / AJAX Sequences / Nested Callbacks
+//
+
+const renderCountry = function (data) {
+  const html = document.createElement("div");
+  html.classList.add("card-container", "col-lg-4", "px-3", "mb-3");
+  html.innerHTML = `
+  <img class="flag w-100 rounded-top" src="${data.flags.svg}" alt="${
+    data.flags.alt
+  }"/>
+  <div class="country-data bg-light px-3 rounded-bottom py-3">
+      <div class="country-name mb-4">
+          <h2 class="name text-uppercase">${data.name.official}</h2>
+          <h3 class="continent">${data.region}</h3>
+      </div>
+      <div class="country-data">
+          <p class="population">🙍‍♂️ ${(+data.population / 1000000).toFixed(
+            1
+          )}</p>
+          <p class="language">🗣 ${Object.values(data.languages)[0]}</p>
+          <p class="currency">💰 ${
+            Object.entries(Object.values(data.currencies)[0])[0][1]
+          }</p>
+      </div>
+  </div>`;
+
+  appContainerEl.appendChild(html);
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX call #1
+  const request = new XMLHttpRequest();
+  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+
+    renderCountry(data);
+
+    // get neighbour country
+    const [neighbour] = data?.borders;
+
+    if (!neighbour) return;
+
+    // AJAX call #2
+    const secondRequest = new XMLHttpRequest();
+    secondRequest.open(
+      "GET",
+      `https://restcountries.com/v3.1/alpha/${neighbour}`
+    );
+    secondRequest.send();
+
+    secondRequest.addEventListener("load", function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2[0]);
+    });
+  });
+};
+
+getCountryAndNeighbour("romania");
